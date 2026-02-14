39c18d3632cabffa918f41196a752f43e158e6e3
Avanish0210 <avanishsingh0210@gmail.com>
2026-02-14 20:29:45 +0530

    learned profiles in spring

---
+++ Module 6/TestingApp/src/main/java/com/example/TestingApp/services/impl/DataServiceImplDev.java
@@ -0,0 +1,16 @@
+package com.example.TestingApp.services.impl;
+
+import com.example.TestingApp.services.DataService ;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("Dev")
+public class DataServiceImplDev implements DataService {
+
+
+    @Override
+    public String getdata() {
+        return "Dev Data";
+    }
+}
--- Module 6/TestingApp/src/test/java/com/example/TestingApp/TestingAppApplicationTests.java
+++ Module 6/TestingApp/src/test/java/com/example/TestingApp/TestingAppApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.TestingApp;
 
+import com.example.TestingApp.services.DataService;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@RequiredArgsConstructor
 @SpringBootTest
 class TestingAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
