0d24a49f96c8f7de06069e4a56ccacc274782d25
Avanish0210 <avanishsingh0210@gmail.com>
2026-05-25 00:21:03 +0530

    Added kubernates and docker

---
+++ learnMultithread/src/main/java/com/example/learnMultithread/LearnMultithreadApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnMultithread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnMultithreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnMultithreadApplication.class, args);
+	}
+
+}
---
+++ learnMultithread/src/test/java/com/example/learnMultithread/LearnMultithreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnMultithread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnMultithreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
